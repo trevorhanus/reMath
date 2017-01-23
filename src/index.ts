4c8b73d07154c1012b72c5a5ef4321b9b78faef3
Trevor Hanus <trevorhanus@gmail.com>
2017-01-22 21:37:20 -0800

    ready to test as an npm module

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,43 @@
-import {observable, ObservableMap, computed, action} from 'mobx';
-import {Cell, Options} from './Cell';
-import * as messages from './Messages';
+import {Graph} from './Graph';
+import {BaseCell, Options} from './BaseCell';
+import {Message} from './Messages';
 
 export default class Remath {
-  @observable private _cells: ObservableMap<Cell>;
-  private _messages: messages.Messages;
-  private _symbolToIdMap: ({[symbol: string]: string});
+  private _graph: Graph;
 
   constructor() {
-    this._cells = observable.map<Cell>();
-    this._messages = new messages.Messages();
-    this._symbolToIdMap = {};
+    this._graph = new Graph();
   }
 
-  @computed
-  get cells(): Cell[] {
-    return this._cells.values();
+  public find(symbolOrId: string): BaseCell {
+    return this._graph.find(symbolOrId);
   }
 
-  find(symbol: string): Cell {
-    const id = this.getId(symbol);
-    return this._cells.get(id);
+  public symbolExists(symbol: string): boolean {
+    return this._graph.symbolExists(symbol);
   }
 
-  findById(id: string): Cell {
-    return this._cells.get(id);
+  public addCell(options: AddCellOptions): BaseCell {
+    return this._graph.addCell(options);
   }
 
-  symbolExists(symbol: string): boolean {
-    const id = this.getId(symbol);
-    return this._cells.has(id);
+  public removeCell(symbol: string): void {
+    this._graph.removeCell(symbol);
   }
 
-  @action
-  fromJSON(calcJSON: any) {
-    // add cells
+  public get messages(): Message[] {
+    return this._graph.messages;
+  }
+
+  public removeMessage(id: string): void {
+    this._graph.removeMessage(id);
+  }
+
+  public fromJSON(calcJSON: any) {
     calcJSON.cells.forEach((cellJSON: Options) => {
       this.addCell(cellJSON)
     });
   }
-
-  @action
-  addCell(options: Options): Cell {
-    const {symbol} = options;
-    if (this.symbolExists(symbol)) {
-      this._messages.add({
-        type: messages.Type.ERROR,
-        content: `Remath: symbol \`${symbol}\` already exists`
-      });
-      return;
-    }
-    const newCell = new Cell(options, this);
-    this.setId(symbol, newCell.id);
-    this._cells.set(newCell.id, newCell);
-    return newCell;
-  }
-
-  @action
-  removeCell(symbol: string) {
-    const cell = this.find(symbol);
-    if (!cell) {
-      return;
-    }
-
-    if (this.cellIsReferencedByOthers(symbol)) {
-      this._messages.add({
-        type: messages.Type.WARNING,
-        content: `Removing cell with symbol \`${symbol}\` whose value is referenced by other cells.`
-      });
-    }
-    const id = this.getId(symbol);
-    this._cells.delete(id);
-  }
-
-  @computed
-  get messages(): messages.Message[] {
-    return this._messages.list;
-  }
-
-  @action
-  removeMessage(id: string): void {
-    this._messages.remove(id);
-  }
-
-  private cellIsReferencedByOthers(symbol: string): boolean {
-    const cells = this._cells.values();
-    return cells.some(cell => {
-      return cell.dependsOn(symbol);
-    });
-  }
-
-  getId(symbol: string): string {
-    return this._symbolToIdMap[symbol];
-  }
-
-  private setId(symbol: string, id: string): void {
-    this._symbolToIdMap[symbol] = id;
-  }
-
-  public updateSymbolToIdMap(oldSymbol: string, newSymbol: string): void {
-    const id = this.getId(oldSymbol);
-    this.setId(newSymbol, id);
-  }
 }
+
+export interface AddCellOptions extends Options {}
